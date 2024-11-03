b06d42ce5e49880180329a4b8ead5593ce8629f2
saphess <saphess@mail.ru>
2024-11-03 17:31:40 +0300

    JSON test

--- src/test/java/ru/netology/rest/PostmanEchoApiTestV1.java
+++ src/test/java/ru/netology/rest/PostmanEchoApiTestV1.java
@@ -1,5 +1,6 @@
 package ru.netology.rest;
 
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
@@ -10,16 +11,18 @@ public class PostmanEchoApiTestV1 {
     public void shouldPost() {
         // Given - When - Then
         // Предусловия
+        String jsonData = "{ \"phone\" : \"IPhone\", \"serial\" : 5505 }";
         given()
                 .baseUri("https://postman-echo.com")
-                .contentType("text/plain; charset=UTF-8")
-                .body("Shop") // отправляемые данные (заголовки и query можно выставлять аналогично)
+                .contentType("application/json")
+                .body(jsonData) // отправляемые данные (заголовки и query можно выставлять аналогично)
                 // Выполняемые действия
                 .when()
                 .post("/post")
                 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("Gun"));
+                .body("json.phone", equalTo("IPhone"))
+                .body("json.serial", equalTo(5505));
     }
 }
